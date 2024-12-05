520bed59f127f662b45a0f5cb4bbb8723954027e
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2024-12-05 15:27:02 +0530

    phrase 3.1
    update the fronte-nd

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom';
+
 
 
 function Header() {
 return (
     <Navbar bg='dark' variant='dark' expand="lg" className="bg-body-black">
         <Container fluid>
-            <LinkContainer to='/'>
+     
                 <Navbar.Brand href="#">Health Sync 2.0</Navbar.Brand>
-            </LinkContainer>
+     
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav
@@ -18,20 +19,26 @@ return (
                     style={{ maxHeight: '100px' }}
                     navbarScroll
                 >
-                    <LinkContainer to='/'>
-                        <Nav.Link> <i className='fas fa-home'></i> Home</Nav.Link>
-                    </LinkContainer>
                     
-                    <LinkContainer to='/login'>
-                    <Nav.Link> <i className='fas fa-sign-in-alt'></i> Login</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to='/register'>
-                    <Nav.Link> <i className='fas fa-user-plus'></i> Register</Nav.Link>
-                    </LinkContainer>
-                    {/* <LinkContainer to='/contact'>
-                    <Nav.Link> <i className='fas fa-envelope'></i> Contact</Nav.Link>
-                    </LinkContainer> */}
-                    
+                    <Nav.Link>
+  <Link to={"/"} style={{ textDecoration: "none", color: "inherit" }}>
+    <i className="fas fa-home"></i> Home
+  </Link>
+</Nav.Link>
+
+<Nav.Link>
+    <Link to={"/login"} style={{ textDecoration: "none", color: "inherit" }}>
+        <i className="fas fa-sign-in-alt"></i> Login
+    </Link>
+</Nav.Link>
+
+<Nav.Link>
+    <Link to={"/register"} style={{ textDecoration: "none", color: "inherit" }}>
+        <i className="fas fa-user-plus"></i> Register
+    </Link>
+</Nav.Link>
+
+            
                     
                     
                     
